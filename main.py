8cbe640e4fd67530cffc6dbc4ccedc2ccee00c8c
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2025-07-02 19:13:08 -0400

    Update import path for main module in main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from video.main import main
+from video.__main__ import main
 
 if __name__ == "__main__":
     main()
