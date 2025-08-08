8f08be13fae033a20c6244439856d1fafef245c6
David <104325852+Cdaprod@users.noreply.github.com>
2025-08-08 11:08:10 -0400

    feat: proxy Go gateway and enable dev TLS

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# entrypoint.sh - render nginx template and start gateway
+#
+# Usage:
+#   /entrypoint.sh
+
 # write snippet files directly into /etc/nginx
 cat >/etc/nginx/proxy_defaults.conf <<'EOF'
 proxy_http_version 1.1;
@@ -26,11 +31,13 @@ EOF
 # defaults for envsubst
 : ${API_HOST:=video-api}
 : ${API_PORT:=8080}
+: ${API_GW_HOST:=api-gateway}
+: ${API_GW_PORT:=8081}
 : ${WEB_HOST:=video-web}
 : ${WEB_PORT:=3000}
 
 # render the template into the real nginx.conf
-envsubst '${API_HOST} ${API_PORT} ${WEB_HOST} ${WEB_PORT}' \
+envsubst '${API_HOST} ${API_PORT} ${API_GW_HOST} ${API_GW_PORT} ${WEB_HOST} ${WEB_PORT}' \
   < /etc/nginx/nginx.tmpl > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
