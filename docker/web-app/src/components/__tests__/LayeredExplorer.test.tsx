bbaa1f26d240562f329dec2412de8a31255720f7
David <104325852+Cdaprod@users.noreply.github.com>
2025-08-09 20:04:57 -0400

    feat: add 2.5D layered explorer

---
+++ docker/web-app/src/components/__tests__/LayeredExplorer.test.tsx
@@ -0,0 +1,28 @@
+import assert from 'node:assert';
+import test from 'node:test';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import LayeredExplorer from '../LayeredFS/LayeredExplorer';
+import { AssetCtx } from '../../providers/AssetProvider';
+
+test('LayeredExplorer renders canvas', () => {
+  const mockCtx = {
+    assets: [],
+    folders: [],
+    foldersLoading: false,
+    view: [],
+    filters: {},
+    setFilters: () => {},
+    vectorSearch: async () => {},
+    move: async () => {},
+    remove: async () => {},
+    refresh: () => {},
+  } as any;
+
+  const html = renderToString(
+    <AssetCtx.Provider value={mockCtx}>
+      <LayeredExplorer />
+    </AssetCtx.Provider>
+  );
+  assert.ok(html.includes('<canvas'));
+});
