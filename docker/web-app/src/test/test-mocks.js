69393a43c96c471dd04a8f102941988228dd56df
David <104325852+Cdaprod@users.noreply.github.com>
2025-08-22 18:29:59 -0400

    fix(web-app): scope module mocks to tests

---
+++ docker/web-app/src/test/test-mocks.js
@@ -0,0 +1,25 @@
+/**
+ * Runtime mocks for optional frontend modules in tests.
+ *
+ * Usage:
+ *   NODE_ENV=test node --require ./src/test/test-mocks.js --test <files>
+ */
+if (process.env.NODE_ENV === 'test') {
+  const Module = require('module');
+  const originalRequire = Module.prototype.require;
+  Module.prototype.require = function (id) {
+    if (id === '@mui/material') {
+      const React = require('react');
+      return new Proxy({}, {
+        get: () => (props) => React.createElement(React.Fragment, null, props && props.children)
+      });
+    }
+    if (id === 'next-auth/react') {
+      return { signIn: async () => {} };
+    }
+    if (id === 'react-devtools-core') {
+      return { connectToDevTools: () => ({}), startServer: () => {} };
+    }
+    return originalRequire.call(this, id);
+  };
+}
